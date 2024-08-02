5ce90ab8be5b9312514e872ee243768308c24785
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-08-02 12:08:41 +0900

    Update and rename CNN_Top.v to CNN_Top.v

--- rtl_v2/CNN_Top.v
+++ rtl_v2/CNN_Top.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/01/2024 01:45:08 PM
-// Design Name: 
-// Module Name: CNN_Top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module CNN_Top(
